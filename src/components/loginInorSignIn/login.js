8a1af6cf680d1dfa4e53d6bad1f40976e4fafe38
Nathan John <nafanjohn@gmail.com>
2023-07-28 15:05:02 +0100

    first commit

---
+++ src/components/loginInorSignIn/login.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { loginUser } from '../../utils';
+import './loginsignin.css';
+import { useState } from 'react';
+import Logout from "./logout";
+
+const Login = ({newUser}, props) => {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [isLoggedin, setIsLoggedin] = useState(false);
+ 
+    const login = async (e) => {
+        e.preventDefault();
+        console.log(username, email, password);
+        await loginUser(username, email, password, newUser)
+        setIsLoggedin(true);
+        window.location.href = "/to-do-app";
+    };
+
+    return (
+        <>
+            <div>
+                <h1>Login </h1>
+                {!isLoggedin ? (
+                    <>
+                        <form action="">
+                            <input
+                                type="text"
+                                onChange={(e) => setUsername(e.target.value)}
+                                value={username}
+                                placeholder="Username"
+                            />
+                            <br  />
+                            <input
+                                type="email"
+                                onChange={(e) => setEmail(e.target.value)}
+                                value={email}
+                                placeholder="Email"
+                            />
+                            <br  />
+                            <input
+                                type="password"
+                                onChange={(e) => setPassword(e.target.value)}
+                                value={password}
+                                placeholder="Password"
+                            />
+                            <br  />
+                            <button type="submit" onClick={login}>
+                                GO
+                            </button>
+                        </form>
+                    </>
+                ) : (
+                    <>
+                        <h1>User is logged in</h1>
+                        <Logout cookie={props.cookie} setIsLoggedin={setIsLoggedin} />                     
+                    </>
+                )}
+            </div>
+        </>
+    );
+}
+ 
+export default Login;
---
+++ src/components/loginInorSignIn/logout.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Logout = (props) => {
+
+        props.e.preventDefault();
+        const logout = async () => {
+            localStorage.removeItem(props.cookie)
+            props.setIsLoggedin(false)
+            window.location.href = "/";
+        };
+        
+        return (
+        <div>
+            <button onClick={ () => logout()}>logout user</button>
+        </div>
+        )
+    };
+
+export default Logout;
